a1af54bc21abcd0ca9aba218d68d073b9506fd29
APIVerve Team <hello@apiverve.com>
2025-03-28 17:55:14 -0500

    Bug Fixes

--- src/apiverve_tenseconverter/apiClient.py
+++ src/apiverve_tenseconverter/apiClient.py
@@ -10,10 +10,10 @@ class TenseconverterAPIClient:
         
         self.api_key = api_key
         self.secure = secure
-        self.base_url = 'https://api.apiverve.com/v1.0.9/tenseconverter'
+        self.base_url = 'https://api.apiverve.com/v1/tenseconverter'
         self.headers = {
             'x-api-key': self.api_key,
-            'auth-mode': 'pypi'
+            'auth-mode': 'pypi-package'
         }
 
     def execute(self, params=None):
